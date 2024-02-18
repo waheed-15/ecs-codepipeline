83db3072cfbe7a45c9865eba4ca0585179bd1c7e
waheed <zamawaheed1506@gmail.com>
2024-02-18 16:21:45 +0530

    modified

--- modules/ecr/variables.tf
+++ modules/ecr/variables.tf
@@ -10,10 +10,6 @@ variable "image_tag" {
   default = "latest"
 }
 
-variable "image_repo_url" {
-  default = "847415613895.dkr.ecr.us-west-2.amazonaws.com/flask-application-demo"
-}
-
 variable "tag_immutability" {
   default = "MUTABLE"
 }
